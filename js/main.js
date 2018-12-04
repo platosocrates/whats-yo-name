bc658d347f043a6098e86d6e468d4b5756e85688
Lee <liam.eakins@outlook.com>
2018-12-04 11:17:43 -0600

    Add js if-then

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var newName = prompt("What is your name?");
+
+if (newName == "Thomas") {
+    alert("Hey " + newName + ", our names are the same!" );
+} else {
+    alert("Too bad our names are different, " + newName + ".");
+}
